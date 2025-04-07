481b264905846f7b5ca97dadbdfc89f1db54a967
Lachlan Whyborn <lachlan.whyborn@gmail.com>
2025-04-07 12:46:45 +1000

    Add JULES soil vars to CABLE gridinfo

--- ancillaries/CABLE/create_new_gridinfo.py
+++ ancillaries/CABLE/create_new_gridinfo.py
@@ -3,6 +3,20 @@
 import xarray
 import argparse
 
+JULES_TO_CABLE_MAPPING = {
+        "soil_albedo"   : "albedo2",
+        "T_CLAY"        : "clay",
+        "T_SILT"        : "silt",
+        "T_SAND"        : "sand",
+        "BD"            : "bch",
+        "sm_wilt"       : "swilt",
+        "sm_crit"       : "sfc",
+        "sm_sat"        : "ssat",
+        "sathh"         : "sucs",
+        "hcon"          : "cnds",
+        "satcon"        : "hyds"
+        }
+
 def _parse_args():
     """Read the command line arguments."""
 
@@ -19,14 +33,8 @@ def _parse_args():
             help="Reference CABLE gridinfo file"
             )
     parser.add_argument(
-            "-l",
-            "--lai",
-            help="File containing the LAI data"
-            )
-    parser.add_argument(
-            "-v",
-            "--vegetation",
-            help="File containing the vegetation data"
+            "--inputdir",
+            help="Directory containing the results of the ANTS ancillary suite"
             )
     parser.add_argument(
             "--invertlat",
@@ -42,23 +50,30 @@ def _parse_args():
 
     return parser.parse_args()
 
-def create_new_gridinfo(RefGridinfo, LAI, VegMap, OutFile, InvertLat):
+def create_new_gridinfo(RefGridinfo, InputDir, OutFile, InvertLat):
     """Take the LAI and vegetation data from the ANTS sources and add them to
     the default CABLE gridinfo file."""
 
+    # Open the reference gridinfo that we're going to write to
     RefGridinfo = xarray.open_dataset(RefGridinfo)
 
-    VegMap = xarray.open_dataset(VegMap)
-    LAI = xarray.open_dataset(LAI)
+    # We know the names of the relevant files in the output directory
+    VegMap = xarray.open_dataset(InputDir + "qrparm.veg.dominant_cci_cable.nc")
+    LAI = xarray.open_dataset(InputDir + "lai_cable.nc")
+    JULESSoil = xarray_open_dataset(InputDir + "qrparm.soil_cci.nc")
 
     # Invert latitudes if required
     if InvertLat:
         VegMap = VegMap.isel(latitude=slice(None, None, -1))
         LAI = LAI.isel(latitude=slice(None, None, -1))
+        JULESSoil = JULESSoil.isel(latitude=slice(None, None, -1))
 
     # Add the variables to original dataset
     RefGridinfo["LAI"].values = LAI["leaf_area_index"].values
     RefGridinfo["iveg"].values = VegMap["iveg"].values
+
+    for JULESVar, CABLEVar in JULES_TO_CABLE_MAPPING.items():
+        RefGridinfo[CABLEVar].values = JULESSoil[JULESVar].values
 
     RefGridinfo.to_netcdf(OutFile, "w")
 
@@ -69,8 +84,7 @@ if __name__ == "__main__":
 
     create_new_gridinfo(
             args.refgridinfo,
-            args.lai,
-            args.vegetation,
+            args.inputdir,
             args.output,
             args.invertlat
             )
